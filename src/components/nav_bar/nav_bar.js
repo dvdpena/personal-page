95a2410fc8f43ed4db810c4c368756817fba2b81
David E. Peña <pexd94.play@gmail.com>
2022-04-25 12:51:50 -0500

    Fonts, small screen behavior and proper size for nav_bar are added.

--- src/components/nav_bar/nav_bar.js
+++ src/components/nav_bar/nav_bar.js
@@ -1,22 +1,39 @@
-import React from "react";
-import work from '../../brief.svg'
-import home from '../../home.svg'
-import contact from '../../contact.svg'
-import software from '../../software.svg'
-import cap from '../../cap.svg'
-import profile from '../../profile.jpg'
+import React, { useContext } from "react";
+import work from '../../res/svgs/brief.svg'
+import home from '../../res/svgs/home.svg'
+import contact from '../../res/svgs/contact.svg'
+import software from '../../res/svgs/software.svg'
+import cap from '../../res/svgs/cap.svg'
+import profile from '../../res/svgs/profile.jpg'
 import style from './nav_bar.module.css'
+import { pageContext } from "../../utils/nav_context";
+import {WORK, SOFT, EDU, CONTACT, HOME, PROFILE, NAME} from '../../res/strings/en'
 
 const NavBar = () =>{
+    const {setPage} = useContext(pageContext);
     return (
         <>
             <div className={style.navBar}>
-                <img className={style.img}src={profile} alt="Profile picture" onClick={()=>{console.log("clicked")}}/>
-                <img className={style.btn} src={home} onClick={()=>{console.log("clicked")}}/>
-                <img className={style.btn} src={work} onClick={()=>{console.log("clicked")}}/>
-                <img className={style.btn} src={software}/>
-                <img className={style.btn} src={cap}/>
-                <img className={style.btn} src={contact}/>
+                <div className={style.profile} onClick={()=>{setPage(HOME)}}>
+                    <img className={style.img} alt={PROFILE} src={profile} />
+                    <p className={style.title}>{NAME}</p>
+                </div>
+                <div className={style.item} onClick={()=>{setPage(WORK)}}>
+                    <img className={style.btn} alt={WORK} src={work} />
+                    <p className={style.title}>{WORK.toUpperCase()}</p>
+                </div>
+                <div className={style.item} onClick={()=>{setPage(SOFT)}}>
+                    <img className={style.btn} alt={SOFT} src={software} />
+                    <p className={style.title}>{SOFT.toUpperCase()}</p>
+                </div>
+                <div className={style.item} onClick={()=>{setPage(EDU)}}>
+                    <img className={style.btn} alt={EDU} src={cap} />
+                    <p className={style.title}>{EDU.toUpperCase()}</p>
+                </div>
+                <div className={style.item} onClick={()=>{setPage(CONTACT)}}>
+                    <img className={style.btn} alt={CONTACT} src={contact} />
+                    <p className={style.title}>{CONTACT.toUpperCase()}</p>
+                </div>
             </div>
         </>
     );
---
+++ src/res/strings/en.js
@@ -0,0 +1,9 @@
+module.exports = {
+    NAME:"David E. Peña",
+    HOME:"Summary",
+    WORK:"Work Experience",
+    SOFT:"Software Skill",
+    EDU:"Education",
+    CONTACT:"Contact",
+    PROFILE:"Profile picture"
+}
